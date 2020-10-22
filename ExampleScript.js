b24ebdb22167f82bd76f71ad955394d405f1f6e8
Will Jantscher <william.jantscher@gmail.com>
2020-10-22 15:34:05 -0600

    fixed stuff

---
+++ ExampleScript.js
@@ -0,0 +1,10 @@
+//to run this in VS Code, open new terminal, 
+    //ensure the terminal location is the folder the file is saved in
+        //if you need to change, CD/location        ex. C:\Users\willi\Desktop\SDI_Course
+//type node <file name to run>
+
+//may need to run 'npm install' to initialize
+
+
+console.log('can you see me?')
+//return 'hello there'
